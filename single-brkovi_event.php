c246770868c25084dbf50103010905b1e89862fb
bogec986 <bogec986@outlook.com>
2026-05-03 16:46:46 +0200

    corected share buttons

--- single-brkovi_event.php
+++ single-brkovi_event.php
@@ -1,9 +1,9 @@
 <?php get_header(); ?>
 
 <main class="container my-5 pt-4">
-    <?php while ( have_posts() ) : the_post(); ?>
+    <?php while (have_posts()) : the_post(); ?>
         <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-            
+
             <!-- HEADER PROJEKTA / KONCERTA -->
             <header class="entry-header mb-5">
                 <nav aria-label="breadcrumb">
@@ -26,11 +26,31 @@
                     <a href="<?php echo get_post_type_archive_link('brkovi_event'); ?>" class="btn btn-outline-secondary rounded-pill px-4">
                         <i class="bi bi-arrow-left me-2"></i> Nazad na turneju
                     </a>
-                    <div class="share-links">
+                    <div class="share-links mt-4">
                         <span class="small text-muted me-2">Podeli:</span>
-                        <a href="https://facebook.com<?php the_permalink(); ?>" target="_blank" class="text-secondary mx-1"><i class="bi bi-facebook"></i></a>
-                        <a href="https://twitter.com<?php the_permalink(); ?>" target="_blank" class="text-secondary mx-1"><i class="bi bi-twitter-x"></i></a>
+
+                        <?php
+                        $share_url   = urlencode(get_permalink());
+                        $share_title = urlencode(get_the_title());
+                        ?>
+
+                        <!-- Facebook Share -->
+                        <a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $share_url; ?>"
+                            target="_blank"
+                            rel="noopener"
+                            class="text-secondary mx-1">
+                            <i class="bi bi-facebook"></i>
+                        </a>
+
+                        <!-- Twitter (X) Share -->
+                        <a href="https://twitter.com/intent/tweet?url=<?php echo $share_url; ?>&text=<?php echo $share_title; ?>"
+                            target="_blank"
+                            rel="noopener"
+                            class="text-secondary mx-1">
+                            <i class="bi bi-twitter-x"></i>
+                        </a>
                     </div>
+
                 </div>
             </footer>
 
